d8b9e7d81e753d3dca830642c33e698f06bfd6b4
00bunny <159643535+00bunny@users.noreply.github.com>
2024-03-23 23:53:34 +0500

    Add files via upload

---
+++ project_04_combining_operators/main.js
@@ -0,0 +1,7 @@
+var result = 3 + 4 * 5;
+console.log(result);
+// Answer is 31
+var resultt = 3 + 4 * 5;
+resultt++;
+console.log(resultt);
+// Answer is 32
---
+++ project_04_combining_operators/main.ts
@@ -0,0 +1,9 @@
+let result = 3 + 4 * 5;
+console.log(result);
+// Answer is 23
+
+let resultt = 3 + 4 * 5;
+resultt++
+
+console.log(resultt);
+// Answer is 24
